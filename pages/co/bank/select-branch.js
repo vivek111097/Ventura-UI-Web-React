d64472aa1c63b5c6e1de2c455b87a86ed38e1b71
Vivek Chaudhari <vivek_chaudhari@ildc-inlp.idealake.com>
2022-09-20 15:01:40 +0530

    build changes

--- pages/co/bank/select-branch.js
+++ pages/co/bank/select-branch.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBankBranch from '../../../components/clientOnboarding/Bank Details/Search Bank Branch/SearchBankBranch.component'
+import Header from "../../../components/global/Header.component";
 
 const selectBranch = () => {
   return (
